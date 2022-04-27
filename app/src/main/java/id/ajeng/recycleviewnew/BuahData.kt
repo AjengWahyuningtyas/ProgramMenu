89f9daac398026553054cd9abe01e0e98da0c89c
Ajeng Wahyuningtyas <ningtyasajeng24@gmail.com>
2022-04-27 09:44:40 +0800

    update misc

--- app/src/main/java/id/ajeng/recycleviewnew/BuahData.kt
+++ app/src/main/java/id/ajeng/recycleviewnew/BuahData.kt
@@ -2,19 +2,25 @@ package id.ajeng.recycleviewnew
 
 object MakananData {
 
-    private val makananNames = arrayOf(
-        "Rendang",
-        "Bakso",
-        "Sate",
-        "Nasi Padang",
-        "Nasi Goreng",
-        "Rawon",
-        "Gado-Gado",
-        "Martabak Manis",
-        "Tempe Mendoan"
+    private val buahNames = arrayOf(
+        "Nanas Palembang",
+        "Apel Washington",
+        "Buah Naga",
+        "Lemon Lokal",
+        "Melon",
+        "Semangka",
+        "Pisang Cavendish",
+        "Jambu Crystal AB"
     )
 
-    private val makananDeskripsi = arrayOf(
+    private val satuan = arrayOf(
+        "pack",
+        "pcs",
+        "kg",
+        "gr"
+    )
+
+    private val buahDeskripsi = arrayOf(
         "Meat (beef, lamb or goat), coconut milk, chilli, ginger, galangal, turmeric, lemongrass, garlic, shallot",
         "Ground beef, tapioca, noodle, rice vermicelli, beef broth, kailan, celery, salted vegetables, fried shallots",
         "Skewered and grilled meats with various sauces, mainly peanut sauce",
@@ -26,27 +32,26 @@ object MakananData {
         "Tempe"
     )
 
-    private val makananPhoto = intArrayOf(
-        R.drawable.rendang,
-        R.drawable.bakso,
-        R.drawable.sate,
-        R.drawable.padang,
-        R.drawable.nasigoreng,
-        R.drawable.rawon,
-        R.drawable.gado,
-        R.drawable.martabakmanis,
-        R.drawable.tempe
+    private val buahPhoto = intArrayOf(
+        R.drawable.nanas,
+        R.drawable.apel,
+        R.drawable.dragonfruit,
+        R.drawable.lemon,
+        R.drawable.melon,
+        R.drawable.watermelon,
+        R.drawable.banana,
+        R.drawable.guava
     )
 
     val listData: ArrayList<Makanan>
     get() {
         var list = arrayListOf<Makanan>()
-            for (position in makananNames.indices){
-                val makanan = Makanan()
-                makanan.name = makananNames[position]
-                makanan.deskripsi = makananDeskripsi[position]
-                makanan.photo = makananPhoto[position]
-                list.add(makanan)
+            for (position in buahNames.indices){
+                val buah = Makanan()
+                buah.name = buahNames[position]
+                buah.deskripsi = buahDeskripsi[position]
+                buah.photo = buahPhoto[position]
+                list.add(buah)
             }
             return list
     }
