1f4ffb44219550d1ecb19aff72e2658d4a1fdb7b
ningtyasajeng24@gmail.com <ningtyasajeng24@gmail.com>
2022-06-05 01:38:02 +0800

    create recipe activity ( not yet implemented )

--- app/src/main/java/id/ajeng/recycleviewnew/Activity/RecipeActivity.kt
+++ app/src/main/java/id/ajeng/recycleviewnew/Activity/RecipeActivity.kt
@@ -2,6 +2,7 @@ package id.ajeng.recycleviewnew.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import id.ajeng.recycleviewnew.R
 
 class RecipeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
