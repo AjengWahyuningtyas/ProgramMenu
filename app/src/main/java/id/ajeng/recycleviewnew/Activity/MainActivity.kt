560d5a671267c57eb18cd1a543dc53bf177c6c2c
ningtyasajeng24@gmail.com <ningtyasajeng24@gmail.com>
2022-06-05 01:30:07 +0800

    update MainActivity to be more functional change with Fragment

--- app/src/main/java/id/ajeng/recycleviewnew/Activity/MainActivity.kt
+++ app/src/main/java/id/ajeng/recycleviewnew/Activity/MainActivity.kt
@@ -1,49 +1,44 @@
-package id.ajeng.recycleviewnew
+package id.ajeng.recycleviewnew.Activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import id.ajeng.recycleviewnew.*
 import id.ajeng.recycleviewnew.Adapter.BuahAdapter
+import id.ajeng.recycleviewnew.Data.BuahData
 import id.ajeng.recycleviewnew.Model.Buah
+import id.ajeng.recycleviewnew.Model.BuahViewsModel
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var rvBuah: RecyclerView
-    private val list = ArrayList<Buah>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        rvBuah = findViewById(R.id.rv_makanan)
-        rvBuah.setHasFixedSize(true)
-
-        list.addAll(BuahData.listData)
-        val firstFragment=FirstFragment()
-        val secondFragment=SecondFragment()
-        val thirdFragment=ThirdFragment()
+        val firstFragment= FirstFragment()
+        val secondFragment= SecondFragment()
+        val thirdFragment= ThirdFragment()
+        val fourthFragment= FourthFragment()
+        val fifthFragment= FifthFragment()
 
         setCurrentFragment(firstFragment)
 
+
         bottomNavigationView.setOnNavigationItemSelectedListener {
             when(it.itemId){
-                // R.id.home->setCurrentFragment(firstFragment)
-                R.id.home->showRecyclerView();
-                R.id.person->setCurrentFragment(secondFragment)
-                R.id.settings->setCurrentFragment(thirdFragment)
+                //R.id.home->setCurrentFragment(firstFragment)
+                R.id.home -> setCurrentFragment(firstFragment) ;
+                R.id.person ->setCurrentFragment(secondFragment)
+                R.id.settings ->setCurrentFragment(thirdFragment)
             }
             true
         }
-        // showRecyclerView()
-    }
-
-    private fun showRecyclerView() {
-        rvBuah.layoutManager= GridLayoutManager( this, 2)
-        val lisBuahAdapter = BuahAdapter.BuahAdapter(list)
-        rvBuah.adapter = lisBuahAdapter
     }
 
     private fun setCurrentFragment(fragment:Fragment)=
