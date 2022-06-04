7d813bc5c0f1dcf789111f34cc5a8ed982d370eb
ningtyasajeng24@gmail.com <ningtyasajeng24@gmail.com>
2022-06-05 01:31:43 +0800

    create second fragment for recipe layout (not yet fixed)

--- app/src/main/java/id/ajeng/recycleviewnew/SecondFragment.kt
+++ app/src/main/java/id/ajeng/recycleviewnew/SecondFragment.kt
@@ -5,6 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import id.ajeng.recycleviewnew.Adapter.BuahAdapter
+import id.ajeng.recycleviewnew.Adapter.ResepAdapter
+import kotlinx.android.synthetic.main.fragment_first.*
+import kotlinx.android.synthetic.main.fragment_second.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,15 +24,15 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class SecondFragment : Fragment(R.layout.fragment_second) {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private val layoutManager: RecyclerView.LayoutManager? = null
+    private val adapter: RecyclerView.Adapter<ResepAdapter.ViewHolder>? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recyclerViewRecipe.apply {
+            layoutManager = GridLayoutManager(activity, 2)
+            adapter = ResepAdapter()
+            addItemDecoration(DividerItemDecoration(activity, GridLayoutManager.HORIZONTAL))
         }
     }
 
@@ -35,25 +42,5 @@ class SecondFragment : Fragment(R.layout.fragment_second) {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_second, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SecondFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SecondFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
