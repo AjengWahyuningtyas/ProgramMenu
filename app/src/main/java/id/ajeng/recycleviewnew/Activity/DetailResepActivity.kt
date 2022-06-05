4ffb6ccd900cf501e3dd41596ca38b1d16d3c2f9
ningtyasajeng24@gmail.com <ningtyasajeng24@gmail.com>
2022-06-05 23:33:35 +0800

    update description and step at resep detail

--- app/src/main/java/id/ajeng/recycleviewnew/Activity/DetailResepActivity.kt
+++ app/src/main/java/id/ajeng/recycleviewnew/Activity/DetailResepActivity.kt
@@ -12,13 +12,19 @@ class DetailResepActivity : AppCompatActivity() {
         setContentView(R.layout.activity_detail_resep)
 
         val recipeName : TextView = findViewById(R.id.namaRecipe)
+        val recipeDescription : TextView = findViewById(R.id.recipeDescription)
+        val recipeDescriptionContent : TextView = findViewById(R.id.descriptionRecipeDetailContent)
         val recipeImage : ImageView = findViewById(R.id.imageDetailRecipe)
 
         val bundle : Bundle?= intent.extras
         val name = bundle!!.getString("Resep")
+        val desc = bundle!!.getString("Deskripsi")
+        val step = bundle!!.getString("Langkah")
         val imageId = bundle!!.getInt("Gambar")
 
         recipeName.text = name
+        recipeDescription.text = desc
+        recipeDescriptionContent.text = step
         recipeImage.setImageResource(imageId)
     }
 }
--- app/src/main/java/id/ajeng/recycleviewnew/Adapter/ResepAdapter.kt
+++ app/src/main/java/id/ajeng/recycleviewnew/Adapter/ResepAdapter.kt
@@ -14,9 +14,60 @@ import id.ajeng.recycleviewnew.R
 
 class ResepAdapter  : RecyclerView.Adapter<ResepAdapter.ViewHolder>() {
 
-    private val listResep = arrayOf("Lapis Pandan Sagu", "Choi Pan")
-    private val deskripsiResep = arrayOf("Lapis Pandan Sagu merupakan makanan asal Betawi", "Rasa bengkuang yang harum dipadukan dengan ebi kering yang nikmat membuat makanan khas Pontianak ini digemari oleh banyak orang")
-    private val imageResep = arrayOf(R.drawable.lapisps, R.drawable.choipan )
+    private val listResep = arrayOf( "Choi Pan", "Bacem Ayam", "PokCoy Siram Cincang Jamur", "Lapis Pandan Sagu")
+    private val descResep = arrayOf( "Rasa bengkuang yang harum dipadukan dengan ebi kering yang nikmat membuat makanan khas Pontianak ini digemari oleh banyak orang", "Engga cuma tempe dan tahun aja loh yang enak dibacem. Resep bacem juga tambah nikmat jika bahan utamanya diubah menjadi ayam. Dicoba yuk resep Bacem Ayamnya!", "Sayur pakcoy yang disiram dengan bumbu jamur. Mudah dan ekonomis untuk dibuat di rumah, bakal bikin puas banget!", "Lapis Pandan Sagu merupakan makanan asal Betawi")
+    private val stepResep = arrayOf(
+        "-3 buah bengkuang ukuran sedang\n"
+        + "-3 sdm ebi kering\n"
+        + "-2 siung bawang putih\n"
+                +"-1/2 sdt merica\n"
+                +"-100gr tepung beras\n"
+                +"-40gr tepung tapioka\n"
+                +"-1 sdm minyak goreng\n"
+                +"-320ml air\n"
+                +"-10 cabe keriting merah\n"
+                +"-8 cabe rawit\n"
+                +"-1 siung bawang putih\n"
+                +"-2 sdm cuka masak\n"
+                +"-2 sdm gula pasir\n"
+                +"-8 siung bawang putih\n",
+        "1 kg ayam potong 8\n"
+                +"-500 ml air kelapa/biasa\n"
+                +"-2 bulatan gula merah\n"
+                +"-3 lembar daun salam\n"
+                +"-1 ruas lengkuas\n"
+                +"-1  sdm garam\n"
+                +"-5 siung bawang merah\n"
+                +"-4 siung bawang putih\n"
+                +"-1 jempol jahe \n"
+                +"-Kunyit sediki\n"
+                +"-1 sdt ketumbar\n",
+        "-250 gr Pakcoy\n"
+                +"- 125 gr daging sapi cincang              \n"
+                +"- 100 gr jamur champignon                 \n"
+                +"- 1 butir bawang bombay                   \n"
+                +"- 2 siung bawang putih                    \n"
+                +"- 2 SDM Saus Tiram                        \n"
+                +"- 1 SDM kecap manis                       \n"
+                +"- 1 SDM kecap asin                        \n"
+                +"- Garam secukupnya                        \n"
+                +"- Kaldu bubuk secukupnya                  \n"
+                +"- Air matang                              \n"
+                +"-1 SDM maizena larutkan dengan sedikit air\n",
+        "-250 gr Pakcoy\n"
+                +"- 125 gr daging sapi cincang              \n"
+                +"- 100 gr jamur champignon                 \n"
+                +"- 1 butir bawang bombay                   \n"
+                +"- 2 siung bawang putih                    \n"
+                +"- 2 SDM Saus Tiram                        \n"
+                +"- 1 SDM kecap manis                       \n"
+                +"- 1 SDM kecap asin                        \n"
+                +"- Garam secukupnya                        \n"
+                +"- Kaldu bubuk secukupnya                  \n"
+                +"- Air matang                              \n"
+                +"-1 SDM maizena larutkan dengan sedikit air\n")
+
+    private val imageResep = arrayOf(R.drawable.choipan, R.drawable.ayambacem, R.drawable.pakcoy, R.drawable.lapisps)
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var listResep: TextView
@@ -49,6 +100,8 @@ class ResepAdapter  : RecyclerView.Adapter<ResepAdapter.ViewHolder>() {
 
             intent.putExtra("Resep", listResep!![position])
             intent.putExtra("Gambar", imageResep!![position])
+            intent.putExtra("Deskripsi", descResep!![position])
+            intent.putExtra("Langkah", stepResep!![position])
 
             context.startActivity(intent)
         }
