be2fba6daf575718b78a6af140d56e65856416a9
ningtyasajeng24@gmail.com <ningtyasajeng24@gmail.com>
2022-06-05 15:27:48 +0800

    update MainActivity

--- app/src/main/java/id/ajeng/recycleviewnew/Activity/MainActivity.kt
+++ app/src/main/java/id/ajeng/recycleviewnew/Activity/MainActivity.kt
@@ -1,8 +1,10 @@
 package id.ajeng.recycleviewnew.Activity
 
 import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.fragment.app.Fragment
@@ -13,11 +15,17 @@ import id.ajeng.recycleviewnew.Data.BuahData
 import id.ajeng.recycleviewnew.Model.Buah
 import id.ajeng.recycleviewnew.Model.BuahViewsModel
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.frame_constraint.*
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var productList: ArrayList<Buah>
+    private lateinit var buahAdapter: BuahAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
