9026d9d1fd5d0531db874b8291f00a726dcde721
ningtyasajeng24@gmail.com <ningtyasajeng24@gmail.com>
2022-06-06 10:53:14 +0800

    set image of profile to be smaller

--- app/src/main/java/id/ajeng/recycleviewnew/Adapter/ProfileAdapter.kt
+++ app/src/main/java/id/ajeng/recycleviewnew/Adapter/ProfileAdapter.kt
@@ -12,40 +12,40 @@ import id.ajeng.recycleviewnew.R
 
 class ProfileAdapter  : RecyclerView.Adapter<ProfileAdapter.ProfileViewHolder>() {
 
-    private val listMenu = arrayOf("Akun", "Sandi", "Privasi", "Keluar")
-    private val listImageMenu = arrayOf(R.drawable.user, R.drawable.login, R.drawable.privacy, R.drawable.exit)
+    private val listProfile = arrayOf("Akun", "Sandi", "Privasi", "Keluar")
+    private val listImageProfile = arrayOf(R.drawable.user, R.drawable.login, R.drawable.privacy, R.drawable.exit)
 
     class ProfileViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         // val imageView: ImageView = itemView.findViewById(R.id.imageProductDetail)
         // val textView: TextView = itemView.findViewById(R.id.productTitle)
-        var listMenu: TextView
+        var listProfile: TextView
         var listImageMenu: ImageView
 
         init {
-            listMenu = itemView.findViewById(R.id.namaBuah)
-            listImageMenu = itemView.findViewById(R.id.imageBuah)
+            listProfile = itemView.findViewById(R.id.namaProfile)
+            listImageMenu = itemView.findViewById(R.id.imageProfile)
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProfileViewHolder {
-        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_card_makanan,parent, false)
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_card_profile,parent, false)
         return ProfileViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        return listMenu.size
+        return listProfile.size
     }
 
     override fun onBindViewHolder(holder: ProfileViewHolder, position: Int) {
-        holder.listMenu.text = listMenu[position]
-        holder.listImageMenu.setImageResource(listImageMenu[position])
+        holder.listProfile.text = listProfile[position]
+        holder.listImageMenu.setImageResource(listImageProfile[position])
 
         holder.listImageMenu.setOnClickListener{
             val context = holder.listImageMenu.context
             val intent = Intent( context, DetailProdukActivity::class.java)
 
-            intent.putExtra("Menu", listMenu!![position])
-            intent.putExtra("Gambar", listImageMenu!![position])
+            intent.putExtra("Menu", listProfile!![position])
+            intent.putExtra("Gambar", listImageProfile!![position])
 
             context.startActivity(intent)
         }
